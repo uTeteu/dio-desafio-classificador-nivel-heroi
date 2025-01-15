0461fe6f1bf39da3f647aaa2faad56a4d53c1c16
Teteu <matheuskiper96@gmail.com>
2025-01-15 19:00:47 -0300

    versão 2

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
